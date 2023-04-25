7ad84512a392bc0932cbc1c5ed808d932f897d71
Aakcayy <ahmetakcay944@gmail.com>
2023-04-25 22:45:32 +0300

    Add project files.

---
+++ Randevu Defteri/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Randevu_Defteri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonRandevuAl = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonRandevular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(646, 420);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(105, 70);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(201, 22);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(-4, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tarih >>";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(-3, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "İsim-Soyisim >>";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // buttonRandevuAl
+            // 
+            this.buttonRandevuAl.Location = new System.Drawing.Point(105, 129);
+            this.buttonRandevuAl.Name = "buttonRandevuAl";
+            this.buttonRandevuAl.Size = new System.Drawing.Size(160, 48);
+            this.buttonRandevuAl.TabIndex = 5;
+            this.buttonRandevuAl.Text = "RandevuAl";
+            this.buttonRandevuAl.UseVisualStyleBackColor = true;
+            this.buttonRandevuAl.Click += new System.EventHandler(this.buttonRandevuAl_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(13, 422);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "AKÇAY.LTİ.ŞTİ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(326, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(472, 100);
+            this.listBox1.TabIndex = 7;
+            // 
+            // buttonRandevular
+            // 
+            this.buttonRandevular.Location = new System.Drawing.Point(476, 129);
+            this.buttonRandevular.Name = "buttonRandevular";
+            this.buttonRandevular.Size = new System.Drawing.Size(159, 48);
+            this.buttonRandevular.TabIndex = 8;
+            this.buttonRandevular.Text = "Randevular";
+            this.buttonRandevular.UseVisualStyleBackColor = true;
+            this.buttonRandevular.Click += new System.EventHandler(this.buttonRandevular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRandevular);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonRandevuAl);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Randevu Defteri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonRandevuAl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonRandevular;
+    }
+}
+
---
+++ Randevu Defteri/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Randevu_Defteri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString();
+        }
+
+        private void buttonRandevuAl_Click(object sender, EventArgs e)
+        {
+            bool randevuonay = true;
+            string FileName = "Randevu Defteri.txt";
+            string[] lines = File.ReadAllLines(FileName);
+            string aranantarih = dateTimePicker1.Text;
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Tüm alanları doldurmak zorunludur.");
+            }
+            if (textBox1.Text != "")
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].Contains(aranantarih))
+                    {
+                        MessageBox.Show(aranantarih + " tarihinde başka bir randevu mevcuttur.Lütfen başka tarihe randevu alınız.");
+                        randevuonay = false;
+                        break;
+                    }
+
+                }
+
+            }
+            if (randevuonay != false && textBox1.Text!="")
+            {
+                if (!File.Exists(FileName))
+                {
+                    FileStream fs = new FileStream(FileName, FileMode.OpenOrCreate);
+
+                    using (StreamWriter sr = new StreamWriter(fs))
+                    {
+                        sr.WriteLine("Sayın" + " " + textBox1.Text + " " + dateTimePicker1.Text);
+
+                    }
+
+                }
+                else
+                {
+                    FileStream fs = new FileStream(FileName, FileMode.Append);
+                    using (StreamWriter sr = new StreamWriter(fs))
+                    {
+                        sr.WriteLine("Sayın" + " " + textBox1.Text + " " + dateTimePicker1.Text);
+                    }
+
+
+                }
+                string appointmentDate = dateTimePicker1.Value.Date.ToString("dd.MM.yyyy");
+                MessageBox.Show("Randevunuz >> " + textBox1.Text + "  " + dateTimePicker1.Text + "  " + "tarihindedir" + "\nİyi Günler");
+            }
+        }
+
+
+
+
+
+
+
+
+        private void buttonRandevular_Click(object sender, EventArgs e)
+        {
+            string FileName = "Randevu Defteri.txt";
+            string appointmentDate = dateTimePicker1.Value.Date.ToString("dd.MM.yyyy");
+            listBox1.Items.Clear();
+            int count = 0;
+            using (StreamReader sr = new StreamReader(FileName)) //Okuma için
+            {
+                while (sr.Peek() > 0) //Erişilebilir karakter sayısı
+                {
+                    count++;
+                    listBox1.Items.Add(count + "." + "Randevu>> " + sr.ReadLine() + "  " + "tarihindedir");
+
+                }
+            }
+        }
+    }
+}
